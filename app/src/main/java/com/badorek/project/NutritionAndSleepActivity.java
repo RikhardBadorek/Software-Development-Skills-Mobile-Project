8fc2acc7455fcf67eaa3a7a39328af14837b9b00
Rikhard <rikhard.badorek@student.lut.fi>
2024-09-13 16:12:43 +0300

    Basic structure of the project is made

---
+++ app/src/main/java/com/badorek/project/NutritionAndSleepActivity.java
@@ -0,0 +1,24 @@
+package com.badorek.project;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class NutritionAndSleepActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_nutrition_and_sleep);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
